6f8c7d9ba457b17316c0ec15dce0451a235bb4e3
oksuna <87907819+oksuna@users.noreply.github.com>
2021-07-24 09:24:51 -0700

    basic code

---
+++ arrayinit/arrayinit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace arrayinit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+                Console.WriteLine(numbers[i]);
+            }
+
+
+            Console.WriteLine("Hello Oksana!");
+        }
+    }
+}
