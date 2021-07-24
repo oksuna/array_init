148f41a9c098d4771a47f5550b51e8bf8626943d
oksuna <87907819+oksuna@users.noreply.github.com>
2021-07-24 09:49:44 -0700

    changeHello

--- arrayinit/arrayinit/Program.cs
+++ arrayinit/arrayinit/Program.cs
@@ -14,7 +14,7 @@ namespace arrayinit
             }
 
 
-            Console.WriteLine("Hello Oksana from Slava!");
+            Console.WriteLine("Hello Oksana from Slava, and kiss!");
         }
     }
 }
